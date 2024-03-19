320e2dedf9d25462ea5dcc2808f4d7acdb75424c
Presh000 <micpresh000@gmail.com>
2024-03-19 09:10:15 +0000

    edit

--- app/page.jsx
+++ app/page.jsx
@@ -11,17 +11,17 @@ const Home = () => {
           className="max-lg:w-[999px]"
         />
         <div>
-          <p className="home_text text-[20px] md:text-[21px] lg:text-[18px] xl:text-[20px] leading-8 md:leading-8  lg:leading-7 xl:leading-8  ">
+          <p className="home_text text-[18px] md:text-[21px] lg:text-[18px] xl:text-[20px] leading-7 md:leading-8  lg:leading-7 xl:leading-8  ">
             Omotola Elatuyi is a tenacious, passionate marketer who loves
             turning bold ideas into impactful & memorable experiences.
           </p>
-          <p className="home_text text-[20px] md:text-[21px] lg:text-[18px] xl:text-[20px] leading-8 md:leading-8  lg:leading-7 xl:leading-8">
+          <p className="home_text text-[18px] md:text-[21px] lg:text-[18px] xl:text-[20px] leading-7 md:leading-8  lg:leading-7 xl:leading-8">
             she has worked in various companies occupying senior marketing
             roles, the companies include Reckitt Benckiser, Guinness and Pladis
             global.
           </p>
-          <p className="home_text text-[20px] md:text-[21px] lg:text-[18px] xl:text-[20px] leading-8 md:leading-8  lg:leading-7 xl:leading-8">
-            {" "}
+          <p className="home_text text-[18px] md:text-[21px] lg:text-[18px] xl:text-[20px] leading-7 md:leading-8  lg:leading-7 xl:leading-8">
+       
             Tola: I am an energetic, visionary and passionate expert in the
             crafting and implementation of consumer-centric and resonant
             marketing strategies. A visionary leader, team player, strong
@@ -29,7 +29,7 @@ const Home = () => {
             also have an impressive track record in leading targeted consumer
             activations through purposeful marketing.
           </p>
-          <p className="home_text text-[20px] md:text-[21px] lg:text-[18px] xl:text-[20px] leading-8 md:leading-8  lg:leading-7 xl:leading-8">
+          <p className="home_text text-[18px] md:text-[21px] lg:text-[18px] xl:text-[20px] leading-7 md:leading-8  lg:leading-7 xl:leading-8">
             she is currently working on an autobiography to be released april
             3rd.
           </p>
